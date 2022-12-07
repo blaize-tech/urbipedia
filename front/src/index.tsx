566d1d252160ab3b4a829c5bc0966ea96052d0e0
newcodepusher <svyatoslav.d@blaize.tech>
2022-12-07 10:24:20 +0200

    files list panel by default is open

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -132,7 +132,7 @@ export function GraphPage() {
 
   let isOpenFilesListSideBar, onOpenFilesListSideBar, onCloseFilesListSideBar;
   {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure({isOpen: true})
     isOpenFilesListSideBar=isOpen;
     onOpenFilesListSideBar=onOpen;
     onCloseFilesListSideBar=onClose;
