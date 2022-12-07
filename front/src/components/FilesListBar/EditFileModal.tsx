9c69e16fe8bbe85f3bbd049493671191bf6b2ec2
newcodepusher <svyatoslav.d@blaize.tech>
2022-12-07 17:41:15 +0200

    implement edit file modal window

--- front/src/components/FilesListBar/EditFileModal.tsx
+++ front/src/components/FilesListBar/EditFileModal.tsx
@@ -1,18 +1,11 @@
 import React, {useContext, useState} from 'react'
-import {Input} from '@chakra-ui/react'
+import {Textarea} from '@chakra-ui/react'
 import {
-    Flex,
-    IconButton,
-    ButtonGroup,
-    Tooltip,
-    Box,
     Modal,
     ModalOverlay,
     ModalContent,
     ModalHeader, ModalCloseButton, ModalBody, VStack, Text, ModalFooter, Button
 } from '@chakra-ui/react'
-import {PlusSquareIcon, EditIcon, DeleteIcon, CalendarIcon} from '@chakra-ui/icons'
-import {deleteNodeInEmacs} from "../../util/webSocketFunctions";
 
 export interface ToolbarProps {
     fileName: string
@@ -32,7 +25,11 @@ export const EditFileModal = (props: ToolbarProps) => {
     } = props;
     const [value, setValue] = useState(content);
     return (
-        <Modal isCentered isOpen={showModal} onClose={() => onClose()}>
+        <Modal
+            isCentered
+            isOpen={showModal}
+            onClose={() => onClose()}
+        >
             <ModalOverlay/>
             <ModalContent zIndex="popover">
                 <ModalHeader>{fileName}</ModalHeader>
@@ -40,7 +37,7 @@ export const EditFileModal = (props: ToolbarProps) => {
                 <ModalBody>
                     <VStack spacing={4} display="flex" alignItems="flex-start">
                         <Text>Edit file:</Text>
-                        <Input type="text" value={value} onChange={(e) => {
+                        <Textarea value={value} onChange={(e) => {
                             setValue(e.target.value);
                         }}/>
                         <Text>Add tags:</Text>
