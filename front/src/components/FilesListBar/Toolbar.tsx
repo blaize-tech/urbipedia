438382fc809845cf33fe962751016b11b9fb3d5e
newcodepusher <svyatoslav.d@blaize.tech>
2022-12-07 10:41:20 +0200

    add icons to toolbar for files lsit panel

--- front/src/components/FilesListBar/Toolbar.tsx
+++ front/src/components/FilesListBar/Toolbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Flex, IconButton, ButtonGroup, Tooltip} from '@chakra-ui/react'
-import {ChevronLeftIcon, ChevronRightIcon} from '@chakra-ui/icons'
+import {PlusSquareIcon, EditIcon, DeleteIcon, CalendarIcon} from '@chakra-ui/icons'
 
 export interface ToolbarProps {
     // setJustification: any
@@ -14,14 +14,48 @@ export const Toolbar = (props: ToolbarProps) => {
         <Flex flex="0 1 40px" pb={3} alignItems="center" justifyContent="space-between" pr={1}>
             <Flex>
                 <ButtonGroup isAttached>
-                    <Tooltip label="Go backward">
+                    <Tooltip label="Create new">
                         <IconButton
                             _focus={{}}
                             variant="subtle"
-                            icon={<ChevronLeftIcon/>}
-                            aria-label="Previous node"
+                            icon={<PlusSquareIcon/>}
+                            aria-label="Create new file"
                             disabled={false}
-                            onClick={() => {}}
+                            onClick={() => {
+                            }}
+                        />
+                    </Tooltip>
+                    <Tooltip label="Edit selected">
+                        <IconButton
+                            _focus={{}}
+                            variant="subtle"
+                            icon={<CalendarIcon/>}
+                            aria-label="Edit selected file"
+                            disabled={true}
+                            onClick={() => {
+                            }}
+                        />
+                    </Tooltip>
+                    <Tooltip label="Rename selected">
+                        <IconButton
+                            _focus={{}}
+                            variant="subtle"
+                            icon={<EditIcon/>}
+                            aria-label="Rename selected file"
+                            disabled={true}
+                            onClick={() => {
+                            }}
+                        />
+                    </Tooltip>
+                    <Tooltip label="Delete selected">
+                        <IconButton
+                            _focus={{}}
+                            variant="subtle"
+                            icon={<DeleteIcon/>}
+                            aria-label="Delete selected file"
+                            disabled={true}
+                            onClick={() => {
+                            }}
                         />
                     </Tooltip>
                 </ButtonGroup>
