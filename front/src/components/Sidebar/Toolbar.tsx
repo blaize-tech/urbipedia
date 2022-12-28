1f688ba47640914ab6b49fd472e6ef659a5fef3b
alexm-blaize <alex.m@blaize.tech>
2022-12-28 17:41:31 +0200

    Refactor template of the Toolbar component

--- front/src/components/Sidebar/Toolbar.tsx
+++ front/src/components/Sidebar/Toolbar.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react';
-import { Flex, IconButton, ButtonGroup, Tooltip } from '@chakra-ui/react'
+import { Tooltip } from '@chakra-ui/react'
 import { BiAlignJustify, BiAlignLeft, BiAlignMiddle, BiAlignRight } from 'react-icons/bi'
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
-import { IoIosListBox, IoMdListBox } from 'react-icons/io'
 
 interface ToolbarProps {
   setJustification: any
@@ -22,49 +21,46 @@ const Toolbar: FC<ToolbarProps> = ({
   canRedo,
 }) => {
   return (
-    <Flex flex="0 1 40px" pb={3} alignItems="center" justifyContent="space-between" pr={1}>
-      <Flex>
-        <ButtonGroup isAttached>
-          <Tooltip label="Go backward">
-            <IconButton
-              _focus={{}}
-              variant="subtle"
-              icon={<ChevronLeftIcon />}
-              aria-label="Previous node"
-              disabled={!canUndo}
-              onClick={() => previousPreviewNode()}
-            />
-          </Tooltip>
-          <Tooltip label="Go forward">
-            <IconButton
-              _focus={{}}
-              variant="subtle"
-              icon={<ChevronRightIcon />}
-              aria-label="Next node"
-              disabled={!canRedo}
-              onClick={() => nextPreviewNode()}
-            />
-          </Tooltip>
-        </ButtonGroup>
-      </Flex>
-      <Flex>
-        <Tooltip label="Justify content">
-          <IconButton
-            variant="subtle"
-            aria-label="Justify content"
-            icon={
-              [
-                <BiAlignJustify key="justify" />,
-                <BiAlignLeft key="left" />,
-                <BiAlignRight key="right" />,
-                <BiAlignMiddle key="center" />,
-              ][justification]
-            }
-            onClick={() => setJustification((curr: number) => (curr + 1) % 4)}
-          />
-        </Tooltip>
-      </Flex>
-    </Flex>
+    <div>
+      <Tooltip label="Go backward">
+        <button
+          aria-label="Previous node"
+          disabled={!canUndo}
+          onClick={previousPreviewNode}
+          type="button"
+        >
+          <ChevronLeftIcon />
+        </button>
+      </Tooltip>
+
+      <Tooltip label="Go forward">
+        <button
+          aria-label="Next node"
+          disabled={!canRedo}
+          onClick={nextPreviewNode}
+          type="button"
+        >
+          <ChevronRightIcon />
+        </button>
+      </Tooltip>
+
+      <Tooltip label="Justify content">
+        <button
+          aria-label="Justify content"
+          onClick={() => setJustification((curr: number) => (curr + 1) % 4)}
+          type="button"
+        >
+          {
+            [
+              <BiAlignJustify key="justify" />,
+              <BiAlignLeft key="left" />,
+              <BiAlignRight key="right" />,
+              <BiAlignMiddle key="center" />,
+            ][justification]
+          }
+        </button>
+      </Tooltip>
+    </div>
   );
 };
 
