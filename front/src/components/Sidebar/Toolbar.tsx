767b662b3f8a3a6a8e812e5b24c637ba10381e3e
alexm-blaize <alex.m@blaize.tech>
2022-12-29 15:00:03 +0200

    Use IconButton in the Toolbar component instead buttons

--- front/src/components/Sidebar/Toolbar.tsx
+++ front/src/components/Sidebar/Toolbar.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Tooltip } from '@chakra-ui/react';
-import { BiAlignJustify, BiAlignLeft, BiAlignMiddle, BiAlignRight } from 'react-icons/bi'
+
+import IconButton from '../IconButton';
 
 import IconUndo from '../../images/icon-undo.svg';
 import IconRedo from '../../images/icon-redo.svg';
@@ -12,12 +13,12 @@ import IconAlignCenter from '../../images/icon-align-center.svg';
 import styles from './Toolbar.module.scss';
 
 interface ToolbarProps {
-  setJustification: any
-  justification: number
-  previousPreviewNode: any
-  canUndo: any
-  nextPreviewNode: any
-  canRedo: any
+  setJustification: (arg: any) => void;
+  justification: number;
+  previousPreviewNode: (arg: any) => void;
+  canUndo: any;
+  nextPreviewNode: (arg: any) => void;
+  canRedo: any;
 }
 
 const Toolbar: FC<ToolbarProps> = ({
@@ -30,47 +31,38 @@ const Toolbar: FC<ToolbarProps> = ({
 }) => {
   return (
     <div className={styles.container}>
-      <Tooltip label="Go backward">
-        <button
-          aria-label="Previous node"
-          className={styles.button}
-          disabled={!canUndo}
-          onClick={previousPreviewNode}
-          type="button"
-        >
-          <img src={IconUndo} alt="Go backward" height="16" width="16" />
-        </button>
-      </Tooltip>
+      <IconButton
+        ariaLabel="Previous node"
+        className={styles.button}
+        disabled={!canUndo}
+        onClick={previousPreviewNode}
+        title="Go backward"
+        icon={IconUndo}
+      />
 
-      <Tooltip label="Go forward">
-        <button
-          aria-label="Next node"
-          className={styles.button}
-          disabled={!canRedo}
-          onClick={nextPreviewNode}
-          type="button"
-        >
-          <img src={IconRedo} alt="Go forward" height="16" width="16" />
-        </button>
-      </Tooltip>
+      <IconButton
+        ariaLabel="Next node"
+        className={styles.button}
+        disabled={!canRedo}
+        onClick={nextPreviewNode}
+        title="Go forward"
+        icon={IconRedo}
+      />
 
-      <Tooltip label="Justify content">
-        <button
-          aria-label="Justify content"
-          className={styles.button}
-          onClick={() => setJustification((curr: number) => (curr + 1) % 4)}
-          type="button"
-        >
-          {
-            [
-              <img key="justify" src={IconAlignJustify} alt="Justify content" height="16" width="16" />,
-              <img key="left" src={IconAlignLeft} alt="Justify content" height="16" width="16" />,
-              <img key="right" src={IconAlignRight} alt="Justify content" height="16" width="16" />,
-              <img key="center" src={IconAlignCenter} alt="Justify content" height="16" width="16" />,
-            ][justification]
-          }
-        </button>
-      </Tooltip>
+      <IconButton
+        ariaLabel="Justify content"
+        className={styles.button}
+        onClick={() => setJustification((curr: number) => (curr + 1) % 4)}
+        title="Justify content"
+        icon={
+          [
+            IconAlignJustify,
+            IconAlignLeft,
+            IconAlignRight,
+            IconAlignCenter,
+          ][justification]
+        }
+      />
     </div>
   );
 };
