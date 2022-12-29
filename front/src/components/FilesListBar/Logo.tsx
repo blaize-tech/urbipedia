a8c3a62b88edd284c90fb71c7edf5a29375292d1
alexm-blaize <alex.m@blaize.tech>
2022-12-29 19:49:53 +0200

    Implement styles to the image of the Logo component

--- front/src/components/FilesListBar/Logo.tsx
+++ front/src/components/FilesListBar/Logo.tsx
@@ -7,7 +7,13 @@ import styles from './Logo.module.scss';
 const Logo: FC = () => {
   return (
     <div className={styles.container}>
-      <img src={ImageLogo} alt="Urbipedia" height="24" width="110" />
+      <img
+        className={styles.image}
+        height="24"
+        width="110"
+        src={ImageLogo}
+        alt="Urbipedia"
+      />
     </div>
   );
 };
