bed3304539bd5cb2292b9ecaaaf18e41cf79fa0a
alexm-blaize <alex.m@blaize.tech>
2022-12-29 14:55:47 +0200

    Implement a component to the icon buttons

---
+++ front/src/components/IconButton.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import { Tooltip } from '@chakra-ui/react';
+
+interface IconButtonProps {
+  ariaLabel?: string;
+  className?: string;
+  disabled?: boolean;
+  onClick: (arg: any) => void;
+  title?: string;
+  icon: string;
+}
+
+const IconButton: FC<IconButtonProps> = ({
+  ariaLabel,
+  className,
+  disabled,
+  onClick,
+  title,
+  icon,
+}, ref) => {
+  return (
+    <Tooltip label={title}>
+      <button
+        aria-label={ariaLabel}
+        className={className}
+        disabled={disabled}
+        onClick={onClick}
+        type="button"
+      >
+        <img src={icon} alt={title} height="16" width="16" />
+      </button>
+    </Tooltip>
+  );
+};
+
+export default IconButton;
