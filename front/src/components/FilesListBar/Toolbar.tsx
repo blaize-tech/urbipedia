21d331450eb459d5e381c91f8272344e76857f56
alexm-blaize <alex.m@blaize.tech>
2023-01-01 20:42:30 +0200

    Remove useless elements from the Toolbar component of FilesListBar

--- front/src/components/FilesListBar/Toolbar.tsx
+++ front/src/components/FilesListBar/Toolbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Flex, IconButton, ButtonGroup, Tooltip} from '@chakra-ui/react'
+import {IconButton, Tooltip} from '@chakra-ui/react'
 import {PlusSquareIcon, EditIcon, DeleteIcon, CalendarIcon} from '@chakra-ui/icons'
 
 export interface ToolbarProps {
@@ -19,50 +19,46 @@ export const Toolbar = (props: ToolbarProps) => {
         haveSelection,
     } = props
     return (
-        <Flex flex="0 1 40px" pb={3} alignItems="center" justifyContent="space-between" pr={1}>
-            <Flex>
-                <ButtonGroup isAttached>
-                    <Tooltip label="Create new">
-                        <IconButton
-                            _focus={{}}
-                            variant="subtle"
-                            icon={<PlusSquareIcon/>}
-                            aria-label="Create new file"
-                            onClick={createNewFile}
-                        />
-                    </Tooltip>
-                    <Tooltip label="Edit selected">
-                        <IconButton
-                            _focus={{}}
-                            variant="subtle"
-                            icon={<CalendarIcon/>}
-                            aria-label="Edit selected file"
-                            disabled={!haveSelection}
-                            onClick={editFile}
-                        />
-                    </Tooltip>
-                    <Tooltip label="Rename selected">
-                        <IconButton
-                            _focus={{}}
-                            variant="subtle"
-                            icon={<EditIcon/>}
-                            aria-label="Rename selected file"
-                            disabled={!haveSelection}
-                            onClick={renameFile}
-                        />
-                    </Tooltip>
-                    <Tooltip label="Delete selected">
-                        <IconButton
-                            _focus={{}}
-                            variant="subtle"
-                            icon={<DeleteIcon/>}
-                            aria-label="Delete selected file"
-                            disabled={!haveSelection}
-                            onClick={deleteFile}
-                        />
-                    </Tooltip>
-                </ButtonGroup>
-            </Flex>
-        </Flex>
+        <div>
+            <Tooltip label="Create new">
+                <IconButton
+                    _focus={{}}
+                    variant="subtle"
+                    icon={<PlusSquareIcon/>}
+                    aria-label="Create new file"
+                    onClick={createNewFile}
+                />
+            </Tooltip>
+            <Tooltip label="Edit selected">
+                <IconButton
+                    _focus={{}}
+                    variant="subtle"
+                    icon={<CalendarIcon/>}
+                    aria-label="Edit selected file"
+                    disabled={!haveSelection}
+                    onClick={editFile}
+                />
+            </Tooltip>
+            <Tooltip label="Rename selected">
+                <IconButton
+                    _focus={{}}
+                    variant="subtle"
+                    icon={<EditIcon/>}
+                    aria-label="Rename selected file"
+                    disabled={!haveSelection}
+                    onClick={renameFile}
+                />
+            </Tooltip>
+            <Tooltip label="Delete selected">
+                <IconButton
+                    _focus={{}}
+                    variant="subtle"
+                    icon={<DeleteIcon/>}
+                    aria-label="Delete selected file"
+                    disabled={!haveSelection}
+                    onClick={deleteFile}
+                />
+            </Tooltip>
+        </div>
     )
 }
